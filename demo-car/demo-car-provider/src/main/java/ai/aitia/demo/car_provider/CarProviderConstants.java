955157ed89038d0a921075d8caea8fd1c594dd5c
borditamas <borditamas@gmail.com>
2019-10-02 13:52:24 +0200

    outdoor provider project init

---
+++ demo-car/demo-car-provider/src/main/java/ai/aitia/demo/car_provider/CarProviderConstants.java
@@ -0,0 +1,30 @@
+package ai.aitia.demo.car_provider;
+
+public class CarProviderConstants {
+	
+	//=================================================================================================
+	// members
+	
+	public static final String BASE_PACKAGE = "ai.aitia";
+	
+	public static final String CREATE_CAR_SERVICE_DEFINITION = "create-car";
+	public static final String GET_CAR_SERVICE_DEFINITION = "get-car";
+	public static final String INTERFACE_SECURE = "HTTPS-SECURE-JSON";
+	public static final String INTERFACE_INSECURE = "HTTP-INSECURE-JSON";
+	public static final String HTTP_METHOD = "http-method";
+	public static final String CAR_URI = "/car";
+	public static final String BY_ID_PATH = "/{id}";
+	public static final String PATH_VARIABLE_ID = "id";
+	public static final String REQUEST_PARAM_KEY_BRAND = "request-param-brand";
+	public static final String REQUEST_PARAM_BRAND = "brand";
+	public static final String REQUEST_PARAM_KEY_COLOR = "request-param-color";
+	public static final String REQUEST_PARAM_COLOR = "color";	
+	
+	//=================================================================================================
+	// assistant methods
+
+	//-------------------------------------------------------------------------------------------------
+	private CarProviderConstants() {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ demo-energy-forecast/demo-energy-forecast-outdoor-provider/src/main/java/ai/aitia/demo/energy/forecast/outdoor/provider/App.java
@@ -0,0 +1,13 @@
+package ai.aitia.demo.energy.forecast.outdoor.provider;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
