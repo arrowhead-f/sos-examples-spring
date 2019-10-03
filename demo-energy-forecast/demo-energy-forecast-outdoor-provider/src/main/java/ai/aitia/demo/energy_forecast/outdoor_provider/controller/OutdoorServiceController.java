17960e7b939f5425df3c3b378e1db2011042f3d8
borditamas <borditamas@gmail.com>
2019-10-03 13:53:09 +0200

    renaming service

--- demo-energy-forecast/demo-energy-forecast-outdoor-provider/src/main/java/ai/aitia/demo/energy_forecast/outdoor_provider/controller/OutdoorServiceController.java
+++ demo-energy-forecast/demo-energy-forecast-outdoor-provider/src/main/java/ai/aitia/demo/energy_forecast/outdoor_provider/controller/OutdoorServiceController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ai.aitia.demo.energy.forecast.common.EFCommonConstants;
+import ai.aitia.demo.energy.forecast.common.EFUtilities;
 import ai.aitia.demo.energy.forecast.common.dto.EnergyDetailsDTO;
 import ai.aitia.demo.energy_forecast.outdoor_provider.service.OutdoorService;
+import eu.arrowhead.common.exception.BadPayloadException;
 
 @RestController
 public class OutdoorServiceController {
@@ -29,6 +31,14 @@ public class OutdoorServiceController {
 	public List<EnergyDetailsDTO> getOutdoorEnergyDetails(@RequestParam(name = EFCommonConstants.OUTDOOR_REQUEST_PARAM_BUILDING) final long building,
 														  @RequestParam(name = EFCommonConstants.OUTDOOR_REQUEST_PARAM_FROM) final long fromTimestamp,
 														  @RequestParam(name = EFCommonConstants.OUTDOOR_REQUEST_PARAM_TO, required = false) Long toTimestamp) {
+		
+		if (fromTimestamp < 0) {
+			throw new BadPayloadException("fromTimestamp cannot be less than zero");
+		}
+		
+		if (fromTimestamp > EFUtilities.nowUTCSeconds() || fromTimestamp > EFUtilities.nowUTCSeconds()) {
+			throw new BadPayloadException("fromTimestamp or toTimestamp cannot be int the future");
+		}
 		
 		toTimestamp = toTimestamp == null || toTimestamp < fromTimestamp ? fromTimestamp : toTimestamp; 
 		
