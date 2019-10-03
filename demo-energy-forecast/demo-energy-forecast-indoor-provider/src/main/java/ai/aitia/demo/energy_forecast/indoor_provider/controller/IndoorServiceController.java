2b7d20a3cd2162b5385692609df6e63bdd443059
borditamas <borditamas@gmail.com>
2019-10-03 15:38:15 +0200

    indoor service impl

--- demo-energy-forecast/demo-energy-forecast-indoor-provider/src/main/java/ai/aitia/demo/energy_forecast/indoor_provider/controller/IndoorServiceController.java
+++ demo-energy-forecast/demo-energy-forecast-indoor-provider/src/main/java/ai/aitia/demo/energy_forecast/indoor_provider/controller/IndoorServiceController.java
@@ -1,27 +1,45 @@
 package ai.aitia.demo.energy_forecast.indoor_provider.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import eu.arrowhead.common.CommonConstants;
+import ai.aitia.demo.energy.forecast.common.EFCommonConstants;
+import ai.aitia.demo.energy.forecast.common.EFUtilities;
+import ai.aitia.demo.energy.forecast.common.dto.EnergyDetailsListDTO;
+import ai.aitia.demo.energy_forecast.indoor_provider.service.IndoorService;
+import eu.arrowhead.common.exception.BadPayloadException;
 
 @RestController
 public class IndoorServiceController {
 	
 	//=================================================================================================
 	// members
-
-	//TODO: add your variables here
+	
+	@Autowired
+	private IndoorService indoorService;
 
 	//=================================================================================================
 	// methods
 
 	//-------------------------------------------------------------------------------------------------
-	@GetMapping(path = CommonConstants.ECHO_URI)
-	public String echoService() {
-		return "Got it!";
+	@GetMapping(path = EFCommonConstants.INDOOR_ENERGY_DETAILS_SERVICE_URI, produces = MediaType.APPLICATION_XML_VALUE)
+	public EnergyDetailsListDTO getOutdoorEnergyDetails(@RequestParam(name = EFCommonConstants.REQUEST_PARAM_BUILDING) final long building,
+														  @RequestParam(name = EFCommonConstants.REQUEST_PARAM_FROM) final long fromTimestamp,
+														  @RequestParam(name = EFCommonConstants.REQUEST_PARAM_TO, required = false) Long toTimestamp) {
+		
+		if (fromTimestamp < 0) {
+			throw new BadPayloadException("fromTimestamp cannot be less than zero");
+		}
+		
+		if (fromTimestamp > EFUtilities.nowUTCSeconds() || fromTimestamp > EFUtilities.nowUTCSeconds()) {
+			throw new BadPayloadException("fromTimestamp or toTimestamp cannot be int the future");
+		}
+		
+		toTimestamp = toTimestamp == null || toTimestamp < fromTimestamp ? fromTimestamp : toTimestamp; 
+		
+		return indoorService.getHourlyEnergyDetails(building, fromTimestamp, toTimestamp);
 	}
-	
-	//-------------------------------------------------------------------------------------------------
-	//TODO: implement here your provider related REST end points
 }
---
+++ demo-energy-forecast/demo-energy-forecast-indoor-provider/src/main/java/ai/aitia/demo/energy_forecast/indoor_provider/service/IndoorService.java
@@ -0,0 +1,45 @@
+package ai.aitia.demo.energy_forecast.indoor_provider.service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import ai.aitia.demo.energy.forecast.common.EFDataService;
+import ai.aitia.demo.energy.forecast.common.dto.EnergyDetailsDTO;
+import ai.aitia.demo.energy.forecast.common.dto.EnergyDetailsListDTO;
+import ai.aitia.demo.energy.forecast.common.dto.EnergyDetailsDTO.Builder;
+
+@Component
+public class IndoorService {
+	
+	//=================================================================================================
+	// members
+	
+	@Autowired
+	private EFDataService dataService;
+	
+	//=================================================================================================
+	// methods
+	
+	//-------------------------------------------------------------------------------------------------
+	public EnergyDetailsListDTO getHourlyEnergyDetails(final long building, final long tsStart, final long tsEnd) {
+		final LocalDateTime start = LocalDateTime.ofInstant(Instant.ofEpochSecond(tsStart), TimeZone.getDefault().toZoneId());
+		final LocalDateTime startHour = start.withMinute(0).withSecond(0);
+		final LocalDateTime end = LocalDateTime.ofInstant(Instant.ofEpochSecond(tsEnd), TimeZone.getDefault().toZoneId());
+		final LocalDateTime endHour = end.withMinute(0).withSecond(0);
+		
+		final List<EnergyDetailsDTO> energyDetails = new ArrayList<>();
+		for (LocalDateTime timestamp = startHour; timestamp.isBefore(endHour) || timestamp.isEqual(endHour); timestamp = timestamp.plusHours(1)) {
+			final Builder energyDetailsDTOBuilder = new EnergyDetailsDTO.Builder(timestamp.toEpochSecond(ZoneOffset.UTC), building);
+			energyDetails.add(energyDetailsDTOBuilder.setOutTemp(dataService.getIndoorTemperature(timestamp))
+								   					 .build());
+		}
+		return new EnergyDetailsListDTO(energyDetails, energyDetails.get(0).getTimestamp(), energyDetails.get(energyDetails.size() - 1).getTimestamp());
+	}
+}
