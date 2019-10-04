bbc74e2413d513243144463f3393499677a9818e
borditamas <borditamas@gmail.com>
2019-10-04 15:35:00 +0200

    forecast service impl

--- demo-energy-forecast/demo-energy-forecast-provider/src/main/java/ai/aitia/demo/energy_forecast/provider/service/EnergyForecastService.java
+++ demo-energy-forecast/demo-energy-forecast-provider/src/main/java/ai/aitia/demo/energy_forecast/provider/service/EnergyForecastService.java
@@ -7,10 +7,12 @@ import java.io.Reader;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -40,7 +42,7 @@ public class EnergyForecastService {
 	//-------------------------------------------------------------------------------------------------
 	public EnergyForecastDTO forecast(final long building, final long forecastedTimestamp) throws IOException, URISyntaxException {
 		final List<String[]> dataSet = updateDataSet(building, forecastedTimestamp);
-		return predict(dataSet);
+		return predict(dataSet, building, forecastedTimestamp);
 	}
 	
 	//=================================================================================================
@@ -80,9 +82,68 @@ public class EnergyForecastService {
 	}
 	
 	//-------------------------------------------------------------------------------------------------
-	private EnergyForecastDTO predict(final List<String[]> dataSet) {
-		//TODO
-		return new EnergyForecastDTO();
+	private EnergyForecastDTO predict(final List<String[]> dataSet, final long building, final long forecastedTimestamp) {
+		final List<EnergyDetailsDTO> convertedDataSet = new ArrayList<>(dataSet.size());
+		for (final String[] record : dataSet) {
+			final EnergyDetailsDTO energyDetails = new EnergyDetailsDTO.Builder(Long.valueOf(record[0]), Long.valueOf(building))
+																	   .setInTemp(Double.valueOf(record[1]))
+																	   .setOutTemp(Double.valueOf(record[2]))
+																	   .setTotal(Double.valueOf(record[3]))
+																	   .setWater(Double.valueOf(record[4]))
+																	   .build();
+			convertedDataSet.add(energyDetails);
+		}		
+		return new EnergyForecastDTO(building, forecastedTimestamp, predictTotalHeat(convertedDataSet, forecastedTimestamp), predictWaterHeat(convertedDataSet, forecastedTimestamp));
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	private double predictTotalHeat(final List<EnergyDetailsDTO> dataSet, final long forecastedTimestamp) {
+		final List<Double> averageTotalHeatConsumptionPerHourOfDay = new ArrayList<>(24);
+		final List<Long> recordCount = new ArrayList<>(24);
+		
+		for (final EnergyDetailsDTO record : dataSet) {
+			final LocalDateTime timestamp = LocalDateTime.ofInstant(Instant.ofEpochSecond(record.getTimestamp()), TimeZone.getDefault().toZoneId());
+			final int hourOfDay = timestamp.getHour();
+			if (recordCount.get(hourOfDay) == null) {
+				recordCount.add(hourOfDay, 1l);;
+				averageTotalHeatConsumptionPerHourOfDay.add(hourOfDay, record.getTotal());
+			} else {
+				recordCount.add(hourOfDay, recordCount.get(hourOfDay) + 1l);
+				averageTotalHeatConsumptionPerHourOfDay.add(hourOfDay, (averageTotalHeatConsumptionPerHourOfDay.get(hourOfDay) + record.getTotal()) / recordCount.get(hourOfDay));
+			}
+		}
+		
+		final int forecastedHour = LocalDateTime.ofInstant(Instant.ofEpochSecond(forecastedTimestamp), TimeZone.getDefault().toZoneId()).getHour();
+		double forecast = 0.0d;
+		for (int i = 0; i <= forecastedHour; i++) {
+			forecast += averageTotalHeatConsumptionPerHourOfDay.get(i);
+		}
+		return forecast;
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	private double predictWaterHeat(final List<EnergyDetailsDTO> dataSet, final long forecastedTimestamp) {
+		final List<Double> averageWaterHeatConsumptionPerHourOfDay = new ArrayList<>(24);
+		final List<Long> recordCount = new ArrayList<>(24);
+		
+		for (final EnergyDetailsDTO record : dataSet) {
+			final LocalDateTime timestamp = LocalDateTime.ofInstant(Instant.ofEpochSecond(record.getTimestamp()), TimeZone.getDefault().toZoneId());
+			final int hourOfDay = timestamp.getHour();
+			if (recordCount.get(hourOfDay) == null) {
+				recordCount.add(hourOfDay, 1l);;
+				averageWaterHeatConsumptionPerHourOfDay.add(hourOfDay, record.getWater());
+			} else {
+				recordCount.add(hourOfDay, recordCount.get(hourOfDay) + 1l);
+				averageWaterHeatConsumptionPerHourOfDay.add(hourOfDay, (averageWaterHeatConsumptionPerHourOfDay.get(hourOfDay) + record.getWater()) / recordCount.get(hourOfDay));
+			}
+		}
+		
+		final int forecastedHour = LocalDateTime.ofInstant(Instant.ofEpochSecond(forecastedTimestamp), TimeZone.getDefault().toZoneId()).getHour();
+		double forecast = 0.0d;
+		for (int i = 0; i <= forecastedHour; i++) {
+			forecast += averageWaterHeatConsumptionPerHourOfDay.get(i);
+		}
+		return forecast;
 	}
 	
 	//-------------------------------------------------------------------------------------------------
