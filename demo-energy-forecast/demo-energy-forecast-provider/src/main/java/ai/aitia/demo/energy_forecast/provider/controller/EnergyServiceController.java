b4e882f9058cbb4277556e4a53df58d01ee6b917
borditamas <borditamas@gmail.com>
2019-10-04 09:29:26 +0200

    rename contoller

--- demo-energy-forecast/demo-energy-forecast-provider/src/main/java/ai/aitia/demo/energy_forecast/provider/controller/EnergyServiceController.java
+++ demo-energy-forecast/demo-energy-forecast-provider/src/main/java/ai/aitia/demo/energy_forecast/provider/controller/EnergyServiceController.java
@@ -1,4 +1,4 @@
-package eu.arrowhead.client.skeleton.provider.controller;
+package ai.aitia.demo.energy_forecast.provider.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import eu.arrowhead.common.CommonConstants;
 
 @RestController
-public class ProviderController {
+public class EnergyServiceController {
 	
 	//=================================================================================================
 	// members
