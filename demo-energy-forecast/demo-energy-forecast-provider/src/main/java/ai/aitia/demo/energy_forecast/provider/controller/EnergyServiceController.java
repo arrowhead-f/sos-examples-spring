8a44aae0150a03a66d28d3d451523de948a7ee10
borditamas <borditamas@gmail.com>
2019-10-04 14:20:45 +0200

    temporary commit

--- demo-energy-forecast/demo-energy-forecast-provider/src/main/java/ai/aitia/demo/energy_forecast/provider/controller/EnergyServiceController.java
+++ demo-energy-forecast/demo-energy-forecast-provider/src/main/java/ai/aitia/demo/energy_forecast/provider/controller/EnergyServiceController.java
@@ -1,9 +1,19 @@
 package ai.aitia.demo.energy_forecast.provider.controller;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import eu.arrowhead.common.CommonConstants;
+import ai.aitia.demo.energy.forecast.common.EFCommonConstants;
+import ai.aitia.demo.energy.forecast.common.EFUtilities;
+import ai.aitia.demo.energy.forecast.common.dto.EnergyForecastDTO;
+import ai.aitia.demo.energy_forecast.provider.service.EnergyForecastService;
+import eu.arrowhead.common.exception.BadPayloadException;
 
 @RestController
 public class EnergyServiceController {
@@ -11,17 +21,19 @@ public class EnergyServiceController {
 	//=================================================================================================
 	// members
 
-	//TODO: add your variables here
+	@Autowired
+	private EnergyForecastService energyForecastService;
 
 	//=================================================================================================
 	// methods
 
 	//-------------------------------------------------------------------------------------------------
-	@GetMapping(path = CommonConstants.ECHO_URI)
-	public String echoService() {
-		return "Got it!";
+	@GetMapping(path = EFCommonConstants.ENERGY_FORECAST_SERVICE_URI, produces = MediaType.APPLICATION_XML_VALUE)
+	public EnergyForecastDTO getEnergyForecastService(@RequestParam(name = EFCommonConstants.REQUEST_PARAM_BUILDING) final long building,
+													  @RequestParam(name = EFCommonConstants.REQUEST_PARAM_KEY_TIMESTAMP) final long timestamp) throws IOException, URISyntaxException {
+		if (timestamp <= EFUtilities.nowUTCSeconds()) {
+			throw new BadPayloadException("timestamp cannot be in the past");
+		}
+		return energyForecastService.forecast(building, timestamp);
 	}
-	
-	//-------------------------------------------------------------------------------------------------
-	//TODO: implement here your provider related REST end points
 }
