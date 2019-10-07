7331877e5c324fc40268a36cd2a94c75d647a524
borditamas <borditamas@gmail.com>
2019-10-07 10:54:26 +0200

    temporary commit

--- demo-energy-forecast/demo-energy-forecast-consumer/src/main/java/ai/aitita/demo/energy_forecast/consumer/EnergyConsumerMain.java
+++ demo-energy-forecast/demo-energy-forecast-consumer/src/main/java/ai/aitita/demo/energy_forecast/consumer/EnergyConsumerMain.java
@@ -1,5 +1,10 @@
 package ai.aitita.demo.energy_forecast.consumer;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -9,15 +14,19 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpMethod;
 
 import ai.aitia.demo.energy.forecast.common.EFCommonConstants;
+import ai.aitia.demo.energy.forecast.common.dto.EnergyForecastDTO;
 import eu.arrowhead.client.library.ArrowheadService;
 import eu.arrowhead.common.CommonConstants;
+import eu.arrowhead.common.SSLProperties;
 import eu.arrowhead.common.dto.shared.OrchestrationFlags.Flag;
 import eu.arrowhead.common.dto.shared.OrchestrationFormRequestDTO;
 import eu.arrowhead.common.dto.shared.OrchestrationFormRequestDTO.Builder;
 import eu.arrowhead.common.dto.shared.OrchestrationResponseDTO;
 import eu.arrowhead.common.dto.shared.OrchestrationResultDTO;
+import eu.arrowhead.common.dto.shared.ServiceInterfaceResponseDTO;
 import eu.arrowhead.common.dto.shared.ServiceQueryFormDTO;
 import eu.arrowhead.common.exception.ArrowheadException;
+import eu.arrowhead.common.exception.InvalidParameterException;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {CommonConstants.BASE_PACKAGE, EFCommonConstants.BASE_PACKAGE})
@@ -28,6 +37,11 @@ public class EnergyConsumerMain implements ApplicationRunner {
 	
     @Autowired
 	private ArrowheadService arrowheadService;
+    
+    @Autowired
+	protected SSLProperties sslProperties;
+    
+    private final Logger logger = LogManager.getLogger(EnergyConsumerMain.class);
     
     //=================================================================================================
 	// methods
@@ -40,46 +54,72 @@ public class EnergyConsumerMain implements ApplicationRunner {
     //-------------------------------------------------------------------------------------------------
     @Override
 	public void run(final ApplicationArguments args) throws Exception {
-		//SIMPLE EXAMPLE OF INITIATING AN ORCHESTRATION
+    	OrchestrationResultDTO orchestrationResult = orchestrate(EFCommonConstants.ENERGY_FORECAST_SERVICE);
+    	System.out.println(orchestrationResult);
+    	EnergyForecastDTO energyForecast = consumeEnergyForecastService(orchestrationResult, 10l, LocalDateTime.now().toEpochSecond(ZoneOffset.UTC));
+    	System.out.println(energyForecast);
+    }
+    
+    //=================================================================================================
+	// assistant methods
+	
+	//-------------------------------------------------------------------------------------------------
+    private OrchestrationResultDTO orchestrate(final String serviceDefinition) {
+    	final ServiceQueryFormDTO serviceQueryForm = new ServiceQueryFormDTO.Builder(serviceDefinition)
+    			.interfaces(getInterface())
+    			.build();
     	
     	final Builder orchestrationFormBuilder = arrowheadService.getOrchestrationFormBuilder();
+    	final OrchestrationFormRequestDTO orchestrationFormRequest = orchestrationFormBuilder.requestedService(serviceQueryForm)
+    			.flag(Flag.MATCHMAKING, true)
+    			.flag(Flag.OVERRIDE_STORE, true)
+    			.build();
     	
-    	final ServiceQueryFormDTO requestedService = new ServiceQueryFormDTO();
-    	requestedService.setServiceDefinitionRequirement("test-service");
+    	final OrchestrationResponseDTO orchestrationResponse = arrowheadService.proceedOrchestration(orchestrationFormRequest);
     	
-    	orchestrationFormBuilder.requestedService(requestedService)
-    							.flag(Flag.MATCHMAKING, false) //When this flag is false or not specified, then the orchestration response cloud contain more proper provider. Otherwise only one will be chosen if there is any proper.
-    							.flag(Flag.OVERRIDE_STORE, true) //When this flag is false or not specified, then a Store Orchestration will be proceeded. Otherwise a Dynamic Orchestration will be proceeded.
-    							.flag(Flag.TRIGGER_INTER_CLOUD, false); //When this flag is false or not specified, then orchestration will not look for providers in the neighbor clouds, when there is no proper provider in the local cloud. Otherwise it will. 
-    	
-    	final OrchestrationFormRequestDTO orchestrationRequest = orchestrationFormBuilder.build();
-    	
-    	OrchestrationResponseDTO response = null;
-    	try {
-    		response = arrowheadService.proceedOrchestration(orchestrationRequest);			
-		} catch (final ArrowheadException ex) {
-			//Handle the unsuccessful request as you wish!
-		}
-    	
-    	//EXAMPLE OF CONSUMING THE SERVICE FROM A CHOSEN PROVIDER
-    	
-    	if (response == null || response.getResponse().isEmpty()) {
-    		//If no proper providers found during the orchestration process, then the response list will be empty. Handle the case as you wish!
+    	if (orchestrationResponse == null) {
+    		logger.debug("No orchestration response received");
+    	} else if (orchestrationResponse.getResponse().isEmpty()) {
+    		logger.debug("No provider found during the orchestration");
+    	} else {
+    		final OrchestrationResultDTO orchestrationResult = orchestrationResponse.getResponse().get(0);
+    		validateOrchestrationResult(orchestrationResult, serviceDefinition);
+    		return orchestrationResult;
     	}
-    	
-    	final OrchestrationResultDTO result = response.getResponse().get(0); //Simplest way of choosing a provider.
-    	
-    	final HttpMethod httpMethod = HttpMethod.GET;//Http method should be specified in the description of the service.
-    	final String address = result.getProvider().getAddress();
-    	final int port = result.getProvider().getPort();
-    	final String serviceUri = result.getServiceUri();
-    	final String interfaceName = result.getInterfaces().get(0).getInterfaceName(); //Simplest way of choosing an interface.
-    	String token = null;
-    	if (result.getAuthorizationTokens() != null) {
-    		token = result.getAuthorizationTokens().get(interfaceName); //Can be null when the security type of the provider is 'CERTIFICATE' or nothing.
+    	throw new ArrowheadException("Unsuccessful orchestration: " + serviceDefinition);
+    }
+    
+    //-------------------------------------------------------------------------------------------------
+    private EnergyForecastDTO consumeEnergyForecastService(final OrchestrationResultDTO orchestrationResult, final long building, final long timestamp) {
+    	final String token = orchestrationResult.getAuthorizationTokens() == null ? null : orchestrationResult.getAuthorizationTokens().get(getInterface());
+		final String[] queryParam = {orchestrationResult.getMetadata().get(EFCommonConstants.REQUEST_PARAM_KEY_BUILDING), String.valueOf(building),
+									 orchestrationResult.getMetadata().get(EFCommonConstants.REQUEST_PARAM_KEY_TIMESTAMP), String.valueOf(timestamp)};
+		
+		return arrowheadService.consumeServiceHTTP(EnergyForecastDTO.class, HttpMethod.valueOf(orchestrationResult.getMetadata().get(EFCommonConstants.HTTP_METHOD)),
+												   orchestrationResult.getProvider().getAddress(), orchestrationResult.getProvider().getPort(), orchestrationResult.getServiceUri(),
+												   getInterface(), token, null, queryParam);
+    }
+    
+    //-------------------------------------------------------------------------------------------------
+    private String getInterface() {
+    	return sslProperties.isSslEnabled() ? EFCommonConstants.INTERFACE_SECURE : EFCommonConstants.INTERFACE_INSECURE;
+    }
+    
+    //-------------------------------------------------------------------------------------------------
+    private void validateOrchestrationResult(final OrchestrationResultDTO orchestrationResult, final String serviceDefinition) {
+    	if (!orchestrationResult.getService().getServiceDefinition().equalsIgnoreCase(serviceDefinition)) {
+			throw new InvalidParameterException("Requested and orchestrated service definition do not match");
 		}
-    	final Object payload = null; //Can be null if not specified in the description of the service.
     	
-    	final String consumedService = arrowheadService.consumeServiceHTTP(String.class, httpMethod, address, port, serviceUri, interfaceName, token, payload, "testkey", "testvalue");
-	}
+    	boolean hasValidInterface = false;
+    	for (final ServiceInterfaceResponseDTO serviceInterface : orchestrationResult.getInterfaces()) {
+			if (serviceInterface.getInterfaceName().equalsIgnoreCase(getInterface())) {
+				hasValidInterface = true;
+				break;
+			}
+		}
+    	if (!hasValidInterface) {
+    		throw new InvalidParameterException("Requested and orchestrated interface do not match");
+		}
+    }
 }
