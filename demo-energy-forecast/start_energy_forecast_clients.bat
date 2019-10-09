3fdf7483cbb77c922a1ae7580d845031bddd2306
borditamas <borditamas@gmail.com>
2019-10-09 14:46:09 +0200

    update start bat

--- demo-energy-forecast/start_energy_forecast_clients.bat
+++ demo-energy-forecast/start_energy_forecast_clients.bat
@@ -8,21 +8,22 @@ SET time_to_sleep=10
 echo Starting Energy Forecast Demo Clients
 
 cd demo-energy-forecast-indoor-provider\target
-START "demo-energy-forecast-indoor-provider" /B "cmd /c javaw -jar demo-energy-forecast-indoor-provider-4.1.3.jar 2>&1"
+START "demo-energy-forecast-indoor-provider" /B "cmd /c START java -jar demo-energy-forecast-indoor-provider-4.1.3.jar 2>&1"
 echo indoor-provider started
 
 cd ..\..\demo-energy-forecast-outdoor-provider\target
-START "demo-energy-forecast-outdoor-provider" /B "cmd /c javaw -jar demo-energy-forecast-outdoor-provider-4.1.3.jar 2>&1"
+START "demo-energy-forecast-outdoor-provider" /B "cmd /c START java -jar demo-energy-forecast-outdoor-provider-4.1.3.jar 2>&1"
 echo outdoor-provider started
 
 cd ..\..\demo-energy-forecast-provider\target
-START "demo-energy-forecast-provider" /B "cmd /c javaw -jar demo-energy-forecast-provider-4.1.3.jar 2>&1"
+START "demo-energy-forecast-provider" /B "cmd /c START java -jar demo-energy-forecast-provider-4.1.3.jar 2>&1"
 echo energy-forecast-provider started
 
+timeout /t %time_to_sleep% /nobreak > NUL
 echo wait...
 
-cd %parent_path%
+echo energy-forecast-consumer started
+cd ..\..\demo-energy-forecast-consumer\target
+START "demo-energy-forecast-consumer" /B "cmd /c START java -jar demo-energy-forecast-consumer-4.1.3.jar 2>&1"
 
 ::Kill self
-title=energyForecastStarter
-FOR /F "tokens=2" %%p in ('"tasklist /v /NH /FI "windowtitle eq energyForecastStarter""') DO taskkill /pid %%p > NUL 2>&1
