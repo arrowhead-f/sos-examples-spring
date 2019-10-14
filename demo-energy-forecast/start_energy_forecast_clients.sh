ebac0c3455e2dd8f558504eb2ba66a7885858034
borditamas <borditamas@gmail.com>
2019-10-14 16:10:01 +0200

    update start.sh

--- demo-energy-forecast/start_energy_forecast_clients.sh
+++ demo-energy-forecast/start_energy_forecast_clients.sh
@@ -8,20 +8,20 @@ time_to_sleep=10s
 echo Starting Energy Forecast Demo Clients
 
 cd demo-energy-forecast-indoor-provider/target
-nohup java -jar $(find . -maxdepth 1 -name demo-energy-forecast-indoor-provider-\*.jar | sort | tail -n1) >/dev/null &
+nohup java -jar $(find . -maxdepth 1 -name demo-energy-forecast-indoor-provider-\*.jar | sort | tail -n1) >/dev/null 2>&1 &
 echo indoor-provider started
 
 
 cd ../../demo-energy-forecast-outdoor-provider/target
-nohup java -jar $(find . -maxdepth 1 -name demo-energy-forecast-outdoor-provider-\*.jar | sort | tail -n1) >/dev/null &
+nohup java -jar $(find . -maxdepth 1 -name demo-energy-forecast-outdoor-provider-\*.jar | sort | tail -n1) >/dev/null 2>&1 &
 echo outdoor-provider started
 
 cd ../../demo-energy-forecast-provider/target
-nohup java -jar $(find . -maxdepth 1 -name demo-energy-forecast-provider-\*.jar | sort | tail -n1) >/dev/null &
+nohup java -jar $(find . -maxdepth 1 -name demo-energy-forecast-provider-\*.jar | sort | tail -n1) >/dev/null 2>&1 &
 echo energy-forecast-provider started
 
-sleep ${time_to_sleep}
 echo wait..
+sleep ${time_to_sleep}
 
 cd ../../demo-energy-forecast-consumer/target
 java -jar $(find . -maxdepth 1 -name demo-energy-forecast-consumer-\*.jar | sort | tail -n1)
