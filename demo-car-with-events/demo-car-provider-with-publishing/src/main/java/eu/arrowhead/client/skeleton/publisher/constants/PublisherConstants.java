1fb4db217f548f62de9d0f859e02ef01635fc6b9
Gabor Majoros <gabormajo@yahoo.com>
2019-10-25 11:26:56 +0200

    requested change: corect constant value

--- demo-car-with-events/demo-car-provider-with-publishing/src/main/java/eu/arrowhead/client/skeleton/publisher/constants/PublisherConstants.java
+++ demo-car-with-events/demo-car-provider-with-publishing/src/main/java/eu/arrowhead/client/skeleton/publisher/constants/PublisherConstants.java
@@ -6,7 +6,7 @@ public class PublisherConstants {
 
 	public static final String START_INIT_EVENT_PAYLOAD= "InitStarted";
 	public static final String START_RUN_EVENT_PAYLOAD= "RunStarted";
-	public static final String PUBLISHR_DESTROYED_EVENT_PAYLOAD= "RunStarted";
+	public static final String PUBLISHR_DESTROYED_EVENT_PAYLOAD= "DestroyStarted";
 
 	
 	//=================================================================================================
