0eeb4871555f8c4aa73c0e4e11f94e8690f37ca2
borditamas <borditamas@gmail.com>
2020-01-02 10:54:09 +0100

    car provider legacy endpoints update

--- demo-car/demo-car-provider/src/main/java/ai/aitia/demo/car_provider/controller/CarServiceController.java
+++ demo-car/demo-car-provider/src/main/java/ai/aitia/demo/car_provider/controller/CarServiceController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +25,6 @@ import ai.aitia.demo.car_provider.entity.Car;
 import eu.arrowhead.common.exception.BadPayloadException;
 
 @RestController
-@RequestMapping(CarProviderConstants.CAR_URI)
 public class CarServiceController {
 	
 	//=================================================================================================
